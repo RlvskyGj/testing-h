a9a06b11d496bff5e0807c5e3816729509bc7912
RlvskyGj <41884064+RlvskyGj@users.noreply.github.com>
2018-07-30 08:48:53 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+import time
+
+token='518126418:AAGQHyepa2vgecgoTbI3zON5kIRijjWbTYw'
+
+bot=telebot.TeleBot(token)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_msg(message):
+	bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
