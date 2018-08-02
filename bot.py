bce0ab45b4bddd962105ab17cd3d7ee7703cfd63
RlvskyGj <41884064+RlvskyGj@users.noreply.github.com>
2018-08-02 13:47:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,20 @@ import telebot
 
 bot = telebot.TeleBot('518126418:AAGQHyepa2vgecgoTbI3zON5kIRijjWbTYw')
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+bot = telebot.TeleBot(API_TOKEN)
 
-if __name__ == '__main__':
-	bot.polling()
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
