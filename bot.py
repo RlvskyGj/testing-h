d96a14f0c99c0d33bf395e14df71518b011cfc3a
RlvskyGj <41884064+RlvskyGj@users.noreply.github.com>
2018-08-02 13:54:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,7 @@ bot = telebot.TeleBot('518126418:AAGQHyepa2vgecgoTbI3zON5kIRijjWbTYw')
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+Привет, я бот Андрей!\
 """)
 
 
